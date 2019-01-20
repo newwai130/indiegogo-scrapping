3ba4d542462e6ad56738fc6cc035ca66ae267460
newwai130 <42857509+newwai130@users.noreply.github.com>
2019-01-21 00:08:10 +0800

    0.5

--- PageContentExtractor.py
+++ PageContentExtractor.py
@@ -2,7 +2,6 @@ import requests
 from pathlib import *
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 from time import sleep
 import traceback
 
@@ -13,29 +12,32 @@ class PageContentExtractor():
 		option.add_argument('--headless')
 		option.add_argument('--log-level=3')
 
-		self.driver = webdriver.Chrome(chrome_options=option)
-		#self.driver.set_page_load_timeout(30)
+		self.driver = webdriver.Chrome(options=option)
 		self.driver.set_window_size(1920, 800)
 		self.imageOutputFolder = Path("image")
 		self.imageOutputFolder.mkdir(parents=True, exist_ok=True)
 		
-	def readingPageInforamtion(self, href, type):
+	def readingPageInforamtion(self, href, type, project_id):
 		if(type == 'funding'):
-			print("transforming funding project")
-			output = self.readItemPageInformationFundingType(href)
+			#print("transforming funding project")
+			output = self.readItemPageInformationFundingType(href, project_id)
 			return output
 		elif(type == 'marketplace'):
-			print("transforming marketplace project")
+			#print("transforming marketplace project")
+			return {}
 		else:
-			print("no project type is found")
+			#print("no project type is found")
+			return {}
 
-	def storeWebToImage(self, href, outputfileName):
-		self.driver.get(href)
+	def storeWebToImage(self, url, outputfileName):
+		print("start to store web image")
+		self.driver.get(url)
 		window_size = self.driver.get_window_size()
 		window_height = window_size["height"]
 		total_scroll_height = 0
 		webPage_height = 1
 
+		
 		while (total_scroll_height <= webPage_height):
 			webPage_height  = self.driver.execute_script("return document.body.scrollHeight")
 			self.driver.execute_script("window.scrollBy(0, "+str(window_height)+");")
@@ -43,11 +45,33 @@ class PageContentExtractor():
 
 			#print(total_scroll_height)
 			sleep(0.5)
+			
+			
+		advertisement_buttons = self.driver.find_elements_by_xpath("/svg[@class='ng-isolate-scope']")
+		if(advertisement_buttons and len(advertisement_buttons) > 0 and advertisement_buttons[0].is_displayed()):
+			advertisement_buttons[0].click()
+
+		click_more_buttons = self.driver.find_elements_by_class_name('readMore')
+		if(click_more_buttons and len(click_more_buttons) > 0 and click_more_buttons[0].is_displayed()):
+			click_more_buttons[0].click()
 		
 		body_div = self.driver.find_element_by_xpath("//body")
-		image_name = outputfileName + ".png"
+		image_name = outputfileName + "_web.png"
 		output_file = self.imageOutputFolder / image_name
-		body_div.screenshot(str(output_file))
+		#body_div.screenshot(str(output_file))
+		element_png = body_div.screenshot_as_png
+		with open(str(output_file), 'wb+') as f:
+			f.write(element_png)
+
+		return image_name
+
+	def storeOwnerImage(self, href, outputfileName):
+		response = requests.get(href)
+		image_name = outputfileName
+		output_file = self.imageOutputFolder / image_name
+		if response.status_code == 200:
+			with open(str(output_file), 'wb') as f:
+				f.write(response.content)
 
 	"""
 	def readItemPageInformationMarketingType(self, url):
@@ -73,13 +97,15 @@ class PageContentExtractor():
 			print()
 	"""
 
-	def readItemPageInformationFundingType(self, url):
-		print("start reading webpage")
+	def readItemPageInformationFundingType(self, url, project_id):
+		print("start browsing " + str(url))
 		info = {}
+		item_owner_name = ""
+		item_owner_image_url = ""
 
 		#re-try 3 times if the web request have no reponse
 		try:
-			print (url)
+			
 			try_times = 0;
 			while(True):
 				try:
@@ -98,6 +124,10 @@ class PageContentExtractor():
 					else:
 						sleep(3)
 			
+			info['url'] = url
+
+			#get product title, the job is handled by the json input data source
+			"""
 			try:
 				item_detail_div = self.driver.find_element_by_class_name('campaignHeaderBasics')
 				item_name_div = item_detail_div.find_element_by_class_name('campaignHeaderBasics-title')
@@ -106,15 +136,35 @@ class PageContentExtractor():
 				print('1.item name: '+item_name)
 			except Exception: 
 				print("retry")
+			"""
 
+			#get name of th creater/owner
 			try:		
 				item_owner_name_div = item_detail_div.find_element_by_class_name('campaignTrust-detailsName')
 				item_owner_name = item_owner_name_div.get_attribute("innerText")
-				info['owner'] = item_owner_name
-				print('2.owner: '+item_owner_name)
+				info['owner_name'] = item_owner_name
+				#print('2.owner name: '+item_owner_name)
 			except Exception: 
 				pass
 
+			#get image of the creater/owner
+			try:		
+				item_owner_image_url_div = item_detail_div.find_element_by_class_name('campaignTrust-avatar')
+				item_owner_image_url = item_owner_image_url_div.get_attribute("src")
+				info['owner_image'] = item_owner_image_url
+				#print('3.owner image url: '+item_owner_image_url)
+			except Exception: 
+				pass
+
+			try:
+				if(item_owner_name != "" and item_owner_image_url != ""):
+					owner_image_name = str(project_id) + "_" + item_owner_name
+					self.storeOwnerImage(item_owner_image_url, owner_image_name)
+					info['owner_image_name'] = owner_image_name
+			except Exception: 
+				pass
+
+			"""
 			try:
 				item_rasied_amount_div = self.driver.find_element_by_class_name('indemandProgress-raisedAmount')
 				item_rasied_amount = item_rasied_amount_div.get_attribute("innerText")
@@ -138,6 +188,7 @@ class PageContentExtractor():
 				print('4.raised percentage: '+item_rasied_percentage+'%')
 			except Exception: 
 				pass
+			
 
 			try:
 				item_rasied_percentage_div = self.driver.find_element_by_class_name('campaignGoalProgress-detailsGoal')
@@ -146,19 +197,23 @@ class PageContentExtractor():
 				print('4.raised percentage: '+item_rasied_percentage+'%')
 			except Exception: 
 				pass
+			"""
 
 			
-			#item_rasied_oringal_amount = item_rasied_percentage_div.get_attribute("innerText").split('$')[1].split('\n')[0].strip()
-			#print('oringal amount: '+item_rasied_oringal_amount)
-
-			#item_goal_div = self.driver.find_element_by_class_name('campaignGoalProgress-detailsGoal-popover')
-			#item_goal = item_goal_div.get_attribute("innerText")
-			#print('goal: '+ item_goal)
+			try:
+				item_goal_div = self.driver.find_element_by_class_name('campaignGoalProgress-detailsGoal')
+				#temp_item_goal_div_text = item_goal_div.get_attribute("innerText").encode('UTF-8').replace('\xa0', ' ').decode('UTF-8')
+				temp_item_goal_div_text = item_goal_div.get_attribute("innerText").encode('UTF-8').replace(b'\xc2\xa0', b' ').decode('UTF-8')
+				item_goal = item_goal_div.get_attribute("innerText").split(" ")[2]
+				info['owner_image_name'] = item_goal
+			except:
+				traceback.print_exc()
+				pass
+			
 				
 		except Exception as e: 
 			print(e)
 		finally:
-			print("end reading webpage")
 			return info
 		
 	def closeDriver(self):
@@ -166,16 +221,16 @@ class PageContentExtractor():
 		
 if __name__ == "__main__":		
 	try:
-		href = 'https://www.indiegogo.com/projects/funcl-wireless-headphones-affordable-awesomeness#/'
+		#href = 'https://www.indiegogo.com/projects/pollyanna-publishing#/' 
+		href = 'https://www.indiegogo.com/projects/the-eyal-vilner-big-band-new-swing-album#/'
 		type = "funding"
-		outputfileName = "testing"
+		outputfileName = "testing.png"
 
 		crawler = PageContentExtractor()    
-		crawler.readingPageInforamtion(href, type)
+		#crawler.readingPageInforamtion(href, type, 123)
 		crawler.storeWebToImage(href, outputfileName)
-		crawler.closeDriver()
 
 	except Exception as e: 
-		print(e)
+		traceback.print_exc()
 	finally:
 		crawler.closeDriver()
