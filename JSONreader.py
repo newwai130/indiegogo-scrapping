fcf7ef16bd6a57d836e329fb557b55b5e1888a44
newwai130 <42857509+newwai130@users.noreply.github.com>
2019-01-20 22:52:18 +0800

    0.2

--- JSONreader.py
+++ JSONreader.py
@@ -12,18 +12,24 @@ class JSONreader():
 			lines = f.readlines()
 			for line in lines:
 				content = json.loads(line)
-				href = 'https://www.indiegogo.com'+content['data']['clickthrough_url']
 				if(content['data']['project_type'] == "campaign"):
-					type = "funding"
-				elif(content['data']['project_type'] == "product"):
-					type = "marketplace"
-				title = content['data']['title']
-				output.append({'title': title, 'href': href, 'type': type})
+					title = content['data']['title']
+					url = 'https://www.indiegogo.com'+content['data']['clickthrough_url']
+					project_id = content['data']['project_id']
+					category = content['data']['category']
+					start_date = content['data']['open_date']
+					close_date = content['data']['close_date']
+					currency = content['data']['currency']
+					amount_of_fund_raised = content['data']['funds_raised_amount']
+					percentage_of_fund_raised = content['data']['funds_raised_percent']
+					project_type = "funding"
+				
+					output.append({'title': title, 'url': url, 'project_id': project_id, 'category': category, 'start_date': start_date, 'close_date': close_date,'currency': currency,'amount_of_fund_raised': amount_of_fund_raised, 'percentage_of_fund_raised': percentage_of_fund_raised,  'project_type': project_type, })
 
 		return output
 
 	def close(self):
-		return
+		return 0
 
 if __name__ == "__main__":		
 	try:
@@ -31,7 +37,7 @@ if __name__ == "__main__":
 		print(str(inputFile))
 		fileReader = JSONreader(inputFile)
 		output = fileReader.readJSONInfo()
-		print(output)
+		print(output[0])
 
 	except Exception as e: 
 		traceback.print_exc()		
--- JSONwriter.py
+++ JSONwriter.py
@@ -6,8 +6,9 @@ import traceback
 class JSONwriter():
 	def __init__(self, outputFile):
 		self.outputFile = outputFile
+		self.outputFile.parent.mkdir(exist_ok=True)
 		self.outputFile.touch(mode=0o777, exist_ok=True)
-		self.f = open(str(self.outputFile), 'a+', newline='',encoding='UTF-8')
+		self.f = self.outputFile.open(mode='a+', newline='',encoding='UTF-8')
 
 	def writeInfo(self, info):
 		data = json.dumps(info)
@@ -15,14 +16,4 @@ class JSONwriter():
 		self.f.write('\r\n')
 
 	def close(self):
-		self.close()
-
-if __name__ == "__main__":
-	try:	
-		info = {"123":{'a':'1','b':'2'},"456":{'a':'1','b':'2'}}
-		outputFile = Path("output") / "2018-12-14.json"
-		print(str(outputFile))
-		fileWriter = JSONwriter(outputFile)
-		fileWriter.writeInfo(info)
-	except Exception as e: 
-		traceback.print_exc()
+		self.f.close()
