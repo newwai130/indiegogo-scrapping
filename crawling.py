1cc414399775dab6169eb4c9d3d6c80f2b648432
newwai130 <42857509+newwai130@users.noreply.github.com>
2019-01-20 18:31:21 +0800

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -4,23 +4,36 @@ from PageContentExtractor import *
 from pathlib import *
 import traceback
 
+inputFile = Path("input") / "Indiegogo_2018-12-14T10_41_01_757Z.json"
+outputFile = Path("output") / "2018-12-14.json"
+
+fileReader = JSONreader(inputFile)
+inputSourceInfo = fileReader.readJSONInfo()
+fileWriter = JSONwriter(outputFile)
+
+crawler = PageContentExtractor()
+
 try:
-	inputFile = Path("input") / "Indiegogo_2018-12-14T10_41_01_757Z.json"
-	outputFile = Path("output") / "2018-12-14.json"
+	
+	for inputSourc in inputSourceInfo:
+		try:
+			info = {}
+			
+			crawlerOutput = crawler.readingPageInforamtion(inputSourc['url'], inputSourc['project_type'], inputSourc['project_id'])
+			
+			web_image_name = crawler.storeWebToImage(inputSourc['url'], str(inputSourc['project_id']))
+			info['web_image_name'] = web_image_name
 
-	fileReader = JSONreader(inputFile)
-	inputPageInfo = fileReader.readJSONInfo()
-	fileWriter = JSONwriter(outputFile)
-
-	crawler = PageContentExtractor()
-	for inputPage in inputPageInfo:
-		info = crawler.readingPageInforamtion(inputPage['href'], inputPage['type'])
-		print("info: ",info)
-		crawler.storeWebToImage(inputPage['href'], inputPage['title'])
-		fileWriter.writeInfo(info)
+			info.update(inputSourc)
+			info.update(crawlerOutput)
+			
+			fileWriter.writeInfo(info)
+		except Exception as e: 
+			traceback.print_exc()
 
 except Exception as e: 
 		traceback.print_exc()
 finally:
 	crawler.closeDriver()
+	fileReader.close()
 	fileWriter.close()
